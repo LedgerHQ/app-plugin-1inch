8515a7bc2f4bd0c1fae091d1830f4ed6eb2b3831
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-09-08 15:42:52 +0200

    Use latest plugin sdk interface

--- src/one_1inch_plugin.c
+++ src/one_1inch_plugin.c
@@ -31,7 +31,7 @@ const uint8_t NULL_ETH_ADDRESS[ADDRESS_LENGTH] = {0x00, 0x00, 0x00, 0x00, 0x00, 
 static void handle_init_contract(void *parameters) {
     ethPluginInitContract_t *msg = (ethPluginInitContract_t *) parameters;
 
-    if (msg->interfaceVersion != ETH_PLUGIN_INTERFACE_VERSION_1) {
+    if (msg->interfaceVersion != ETH_PLUGIN_INTERFACE_VERSION_LATEST) {
         msg->result = ETH_PLUGIN_RESULT_UNAVAILABLE;
         return;
     }
