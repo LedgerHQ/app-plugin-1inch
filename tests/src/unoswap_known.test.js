16b2583d9fedc9294de25e82f1c92538291bec2f
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2022-04-13 17:13:13 +0200

    test(unoswap): Rewrite the UnoSwap test, created the generate plugin config, created abis folder and b2c.json file

--- tests/src/unoswap_known.test.js
+++ tests/src/unoswap_known.test.js
@@ -1,16 +1,23 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import { waitForAppScreen, zemu } from './test.fixture';
+import { processTest } from "./test.fixture";
 
-test('Unoswap know token swap', zemu(async (sim, eth) => {
-    // Known ERC20 to known ERC20 (726.7 BOND to 25392 USDC)
-    // https://etherscan.io/tx/0x30517d1dab4b5a24bf8298b3a6111743e6801ace0baf915f08db93b828395998
-    const tx = eth.signTransaction(
-        "44'/60'/0'/0/0",
-        'f8ec82035885098bca5a00830257129411111112542d85b3ef69ae05771c2dccff4faa2680b8c42e95b6c80000000000000000000000000391d2021f89dc339f60fff84546ea23e337750f000000000000000000000000000000000000000000000027657afc260bde280000000000000000000000000000000000000000000000000000000005e1eeb6f70000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000100000000000000003b6d03406591c4bcd6d7a1eb4e537da8b78676c1576ba244018080',
-    );
-    await waitForAppScreen(sim);
-    await sim.navigateAndCompareSnapshots('.', 'unoswap_known', [4, 0]);
+const contractName = "AggregationRouterV4";
 
-    await tx;
-}));
+const testLabel = "unoswapKnowTokenSwap"; // <= Name of the test
+const testDirSuffix = "unoswap_known"; // <= directory to compare device snapshots to
+const testNetwork = "ethereum";
+const signedPlugin = false;
+
+// From : https://etherscan.io/tx/0x5a9e6b1a2be905195fdb98094b336acb3bbad3be5af305ae7431737800a6ddb5
+const rawTx = "0x02f90138018203c585012a05f200850ce3b0fcf9830285fd941111111254fb6c44bac0bed2854e76f90643097d80b8c82e95b6c80000000000000000000000004fb721ef3bf99e0f2c193847afa296b9257d3c3000000000000000000000000000000000000000000000000000000641154cde000000000000000000000000000000000000000000000000000cf5439c3a7c857e0000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000140000000000000003b6d0340cce561cd3217dc5a6f9b9919fd3928cf3d0e282fe26b9977c001a08080ec840ad593dca3c30d371bb496038fd4df08909ab6937737569ab2e130e3a03136d79e29e0fc470177ea91cfc768c1df77fd206abd405d3fc07ef9c4f45ea4";
+
+const devices = [
+    {
+        name: "nanos",
+        label: "Nano S",
+        steps: 9, // <= Define the number of steps for this test case and this device
+    }
+];
+
+devices.forEach((device) =>
+    processTest(device, contractName, testLabel, testDirSuffix, rawTx, signedPlugin, "", testNetwork)
+);
