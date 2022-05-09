1edd54284cab17c087c9c553cc766c2f5eb557ff
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2022-05-09 16:10:45 +0200

    fix(zemu): updated zemu version from 0.25.1 to 0.29.0

--- tests/build_local_test_elfs.sh
+++ tests/build_local_test_elfs.sh
@@ -3,7 +3,6 @@
 # FILL THESE WITH YOUR OWN SDKs PATHS and APP-ETHEREUM's ROOT
 NANOS_SDK=$NANOS_SDK
 NANOX_SDK=$NANOX_SDK
-NANOSP_SDK=$NANOS2_SDK
 APP_ETHEREUM=/plugin_dev/app-ethereum
 
 # create elfs folder if it doesn't exist
@@ -40,19 +39,5 @@ make clean BOLOS_SDK=$NANOX_SDK
 make -j DEBUG=1 BOLOS_SDK=$NANOX_SDK CHAIN=ethereum BYPASS_SIGNATURES=1 ALLOW_DATA=1
 cd -
 cp "${APP_ETHEREUM}/bin/app.elf" "tests/elfs/ethereum_nanox.elf"
-
-echo "*Building elfs for Nano SP..."
-
-echo "**Building app-1inch for Nano SP..."
-make clean BOLOS_SDK=$NANOSP_SDK
-make -j DEBUG=1 BOLOS_SDK=$NANOSP_SDK
-cp bin/app.elf "tests/elfs/1inch_nanosp.elf"
-
-echo "**Building app-ethereum for Nano SP..."
-cd $APP_ETHEREUM
-make clean BOLOS_SDK=$NANOSP_SDK
-make -j DEBUG=1 BOLOS_SDK=$NANOSP_SDK CHAIN=ethereum BYPASS_SIGNATURES=1 ALLOW_DATA=1
-cd -
-cp "${APP_ETHEREUM}/bin/app.elf" "tests/elfs/ethereum_nanosp.elf"
 
 echo "done"
