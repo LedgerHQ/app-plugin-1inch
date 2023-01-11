bc09ec8a023919424be6ee3097bd605bd531c65d
Guilane DENGUIR <83951892+GuilaneDen@users.noreply.github.com>
2023-01-11 16:52:10 +0000

    fix(Swap v5): Added the missing "Partial fill" screen

--- tests/src/V5/Ethereum/swap_known_to_known_ethereum.test.js
+++ tests/src/V5/Ethereum/swap_known_to_known_ethereum.test.js
@@ -19,17 +19,17 @@ const devices = [
     {
         name: "nanos",
         label: "Nano S",
-        steps: 10, // <= Define the number of steps for this test case and this device
+        steps: 11, // <= Define the number of steps for this test case and this device
     },
     {
         name: "nanox",
         label: "Nano X",
-        steps: 6, // <= Define the number of steps for this test case and this device
+        steps: 7, // <= Define the number of steps for this test case and this device
     },
     {
         name: "nanosp",
         label: "Nano S+",
-        steps: 6, // <= Define the number of steps for this test case and this device
+        steps: 7, // <= Define the number of steps for this test case and this device
     }
 ];
 
