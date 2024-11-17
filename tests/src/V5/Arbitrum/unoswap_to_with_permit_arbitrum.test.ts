5a0db0757e6dcbdfd8fac610eff4773dfe00ed3c
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2024-11-17 17:26:15 +0100

    test(LDG-497): updated snapshots

--- tests/src/V5/Arbitrum/unoswap_to_with_permit_arbitrum.test.ts
+++ tests/src/V5/Arbitrum/unoswap_to_with_permit_arbitrum.test.ts
@@ -18,7 +18,7 @@ const devices = [
     {
         name: "nanos",
         label: "Nano S",
-        steps: 13, // <= Define the number of steps for this test case and this device
+        steps: 12, // <= Define the number of steps for this test case and this device
     },
     {
         name: "nanox",
